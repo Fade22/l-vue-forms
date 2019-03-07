2a771f08fd6110f8c64947a7a1535891f142df64
Michael <michael@michael>
2019-03-07 09:26:13 +0900

    Finish fruit mixins

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>
+            Filters & Mixins
+          </h1>
+          <p>{{ text | toUppercase | to-lowercase }}</p>
+          <hr>
+          <button @click="fruits.push('Berries')">Add new item</button>
+          <input v-model="filterText">
+          <ul>
+            <li v-for="fruit in filteredFruits" :key="fruit.id">{{ fruit }}</li>
+          </ul>
+          <hr>
+          <app-list>
+
+
+          </app-list>
+
+      
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-  
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+import List from './List.vue';
+import { fruitMixin } from './fruitMixin';
 
+export default {
+  mixins: [fruitMixin],
+  data() {
+    return {
+      text: 'Hello there!',
+    }
+  },
+  filters: {
+    toUppercase(value){
+      return value.toUpperCase();
+    }
+  },
+  components: {
+    appList: List
+  }
+};
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>         
+          <input v-model="filterText">
+          <ul>
+            <li v-for="fruit in filteredFruits" :key="fruit.id">{{ fruit }}</li>
+          </ul>
+  </div>
+</template>
+<script>
+import { fruitMixin } from './fruitMixin';
+
+export default {
+mixins: [fruitMixin]
+}
+</script>
+
+<style>
+</style>
